f439fe2ffbef399db03599de3f8ef599af0276a1
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-09-09 18:52:07 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -10,4 +10,27 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  
+  
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do 
+    binding.pry
+    Article.create(params[:id], params[:title, params[:content]])
+    erb :show
+  end
+  
 end
